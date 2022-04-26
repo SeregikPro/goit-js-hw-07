c07fc7fddc19c4ac7de37710b9a70a50a7862516
Sergii Proiava <seregikpro@gmail.com>
2022-04-26 18:53:35 +0300

    first commit

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const elGalleryMarkup = createElGalleryMarkup(galleryItems);
+
+function createElGalleryMarkup (images) {
+    return images.map(image =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join(''); 
+};
+
+gallery.innerHTML = elGalleryMarkup;
