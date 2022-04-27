f5275fadf3afe555b6cd1f2c53a7fa73fe184af5
Sergii Proiava <seregikpro@gmail.com>
2022-04-27 08:18:05 +0300

    third commit

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,31 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const elGalleryMarkup = createElGalleryMarkup(galleryItems);
+
+function createElGalleryMarkup (images) {
+    return images.map(image =>
+        `<a class="gallery__item" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      alt="${image.description}"
+    />
+  </a>
+</div>`).join(''); 
+};
+
+gallery.innerHTML = elGalleryMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', 
+{docClose: false,
+    captionsData: "alt",
+    captionPosition: "bottom",
+    captionDelay: 250,
+});
+
+
+
+
